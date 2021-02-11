1f4c09c8414f2154f211968319e17d5b43e2f4ca
Thomas Maitret Richard <thomas.maitret@laposte.net>
2021-02-11 21:52:01 +0100

    try to remove preview templates

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,17 +2,5 @@ import CMS from "netlify-cms-app";
 import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
 
-import BlogPostPreview from "./preview-templates/BlogPostPreview";
-import PagePreview from "./preview-templates/PagePreview";
-import GalleryPreview from "./preview-templates/GalleryPreview";
-
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
-
-CMS.registerPreviewTemplate("index", PagePreview);
-CMS.registerPreviewTemplate("asso", PagePreview);
-CMS.registerPreviewTemplate("permaculture", PagePreview);
-CMS.registerPreviewTemplate("potager", PagePreview);
-CMS.registerPreviewTemplate("offres", PagePreview);
-CMS.registerPreviewTemplate("galerie", GalleryPreview);
-CMS.registerPreviewTemplate("blog", BlogPostPreview);
